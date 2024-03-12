c01f8ae1f9a3c75a2fb05193deb6332524c4a270
kota-karthik <kotakarthik2307@gmail.com>
2024-03-12 17:13:12 +0530

    added likePost,Commentpost controllers

--- Controllers/Post.js
+++ Controllers/Post.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import Post from "../model/postModel.js";
-
+import Comment from "../model/commentModel.js";
 export const newPost = async (req,res)=>{
     try{
         const postedBy=req.params.id;
@@ -22,3 +22,59 @@ export const newPost = async (req,res)=>{
         return { error: "Internal server error !!" };
       }
 }
+
+export const likePost = async (req, res) => {
+  try {
+    const postId = req.params.postId;
+    const userId = req.params.userId;
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      throw new Error("Post not found!!");
+    }
+
+    // Append userId to the likedBy array
+    post.likedBy.push(userId);
+
+    // Update the post with the new likedBy array
+    await Post.findByIdAndUpdate(postId, { likedBy: post.likedBy });
+
+    res.status(200).json({ message: "Post liked successfully" });
+  } catch (error) {
+    console.error("Error while liking post:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const commentPost = async (req, res) => {
+  try {
+    const postId = req.params.postId;
+    const userId = req.params.userId;
+    const comment = req.body.comment;
+
+    const newComment = await Comment.create({
+      post: postId,
+      commentedBy: userId,
+      comment: comment,
+    });
+
+    if (!newComment) {
+      throw new Error("Failed to add new comment!");
+    }
+
+    const commentId = newComment._id;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      throw new Error("Post not found!");
+    }
+
+    post.comments.push(commentId);
+    await Post.findByIdAndUpdate(postId, { comments: post.comments });
+
+    res.status(200).json({ message: "Comment added successfully" });
+  } catch (error) {
+    console.error("Error while adding comment:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- routes/Post.js
+++ routes/Post.js
@@ -1,11 +1,15 @@
 import express from "express";
 const router = express.Router();
 import {
-    newPost
+    newPost,
+    likePost,
+    commentPost
 } from "../Controllers/Post.js";
 
 
 router.post("/newPost/:id", newPost);
+router.patch("/likePost/:userId/:postId",likePost);
+router.patch("/commentPost/:userId/:postId",commentPost);
 
 
 export default router;
