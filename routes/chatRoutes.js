6d97be2646b9a9be661d20ba8977f4c89477ee45
kota-karthik <kotakarthik2307@gmail.com>
2024-07-08 18:04:40 +0530

    added routes for chatRouter

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,5 +1,12 @@
 const express=require("express");
+const protect=require("../middlewares/authMiddleware");
 const router = express.Router();
 
+router.post("/accessChat",protect,accessChat);
+router.get("/fetchChats",protect,fetchChats);
+router.post("/createGroupChat",protect,createGroupChat);
+router.put("/renameGroup",protect,renameGroup);
+router.put("/removeFromGroup",protect,removeFromGroup);
+router.put("/addToGroup",protect,addToGroup);
 
 module.exports=(router);
