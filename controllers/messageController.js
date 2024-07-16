74680744e1d2ae5f76da5d7ff1983c62bb472c11
kota-karthik <kotakarthik2307@gmail.com>
2024-07-16 19:58:43 +0530

    added messageControllers

---
+++ controllers/messageController.js
@@ -0,0 +1,16 @@
+const Message=require('../models/messageModel');
+const User = require("../models/userModel");
+const Chat = require("../models/chatModel");
+const asyncHandler = require("express-async-handler");
+
+const sendMessage=asyncHandler(async(req,res)=>{
+    
+})
+
+const getAllMessages=asyncHandler(async(req,res)=>{
+    
+})
+module.exports={
+    sendMessage,
+    getAllMessages,
+};
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,5 +1,12 @@
 const express=require('express');
 const router = express.Router();
+const {
+    sendMessage,
+    getAllMessages
+}=require('../controllers/messageController');
+const { protect } = require('../middlewares/authMiddleware');
 
+router.route('/sendMessage',protect,sendMessage);
+router.route('/getAllMessages/:chatId',protect,getAllMessages);
 
 module.exports = router;
